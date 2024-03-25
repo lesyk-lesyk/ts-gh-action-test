fccc203d81e28c7b97f3f6d3d5cf31def0ae85d9
Lesyk <oleksiy.kachynskyy@gmail.com>
2024-03-25 18:44:53 +0200

    add stub response

--- fake-api-server/fake-api-server.ts
+++ fake-api-server/fake-api-server.ts
@@ -3,11 +3,49 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const stubResponseStatus = {
+  id: 'test-push-id',
+  remoteId: 'test-remote-id',
+  commit: {
+    message: 'test-commit-message',
+    branchName: 'test-branch-name',
+    author: {
+      name: 'test-author-name',
+      email: 'test-author-email',
+      image: null
+    }
+  },
+  remote: { commits: [] },
+  isOutdated: false,
+  isMainBranch: false,
+  hasChanges: true,
+  status: {
+    preview: {
+      scorecard: [],
+      deploy: {
+        url: 'https://preview-test-url',
+        status: 'success'
+      }
+    },
+    production: {
+      scorecard: [],
+      deploy: {
+        url: 'https://production-test-url',
+        status: 'success'
+      }
+    }
+  }
+};
+
 app.get('*', (req, res) => {
-  res.json({ test: 'test' });
+  res.json(stubResponseStatus);
 });
+
 app.post('*', (req, res) => {
-  res.json({ id: 'test-id-01', mountPath: 'test-mount-path' });
+  res.json({
+    id: 'test-id-01',
+    mountPath: 'test-mount-path'
+  });
 });
 
 app.listen(port, () => {
