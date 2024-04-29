147be9779e122b9823ac8018868cfa0e2558e10e
Lesyk <oleksiy.kachynskyy@gmail.com>
2024-04-29 18:10:49 +0300

    upd

--- src/helpers.ts
+++ src/helpers.ts
@@ -111,7 +111,7 @@ function getCommitSha(): string | undefined {
 
   if (github.context.eventName === 'pull_request') {
     if (github.context.payload.action === 'opened') {
-      return github.context.payload.pull_request?.base?.head?.repo?.sha;
+      return github.context.payload.pull_request?.head?.sha;
     }
     if (github.context.payload.action === 'synchronize') {
       return github.context.payload.after;
