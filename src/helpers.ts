21b1944c5b88d405ed02cee0c365e570adae9aa7
Lesyk <oleksiy.kachynskyy@gmail.com>
2024-04-29 13:41:45 +0300

    stringify debug log

--- src/helpers.ts
+++ src/helpers.ts
@@ -30,7 +30,8 @@ export function parseInputData(): ParsedInputData {
 }
 
 export function parseEventData(): ParsedEventData {
-  console.log('>>>> github.context', JSON.stringify(github.context, null, 2));
+  console.log('>>> github.context', JSON.stringify(github.context, null, 2));
+  console.log('>>> --------------');
   const namespace = github.context.payload?.repository?.owner?.login;
   const repository = github.context.payload?.repository?.name;
 
